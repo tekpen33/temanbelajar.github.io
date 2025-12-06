e5690259d0cd74c28d2abd61ad8bd51ca3de9d7d
Teguh Daviq Nurarif <teguhnubageur@gmail.com>
2025-12-06 08:58:17 -1200

    js restore lesson1

--- kuncilesson1.js
+++ kuncilesson1.js
@@ -61,4 +61,30 @@ btnSelesai.addEventListener("click", function () {
     localStorage.setItem("lesson1_done", "true");
     alert("Pertemuan 1 telah ditandai selesai!");
 });
+// === OPEN DEVELOPER PANEL (CTRL + ALT + D) ===
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.altKey && e.key === 'd') {
+        const panel = document.getElementById("devPanel");
+        panel.style.display = (panel.style.display === "none") ? "block" : "none";
+    }
+});
+
+// === RESET PROGRESS BUTTON ===
+document.addEventListener("DOMContentLoaded", function() {
+    const btnReset = document.getElementById("btnResetDev");
+
+    if (btnReset) {
+        btnReset.addEventListener("click", function() {
+            if (confirm("Reset semua progres siswa?")) {
+
+                localStorage.removeItem("lesson1_done");
+                localStorage.removeItem("lesson2_done");
+                localStorage.removeItem("lesson3_done");
+
+                alert("Progres berhasil direset!");
+                location.reload();
+            }
+        });
+    }
+});
 
